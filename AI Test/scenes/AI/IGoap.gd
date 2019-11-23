75ac8018d2699038fb09ca70dee7b8e98477acc6
Leo Basov <basov_leo@web.de>
2019-11-23 12:03:29 +0100

    Added IGoap interface.

---
+++ AI Test/scenes/AI/IGoap.gd
@@ -0,0 +1,74 @@
+extends Node
+
+"""
+ * Collect the world data for this Agent that will be
+ * used for GOAP planning.
+"""
+#using System.Collections.Generic;
+
+
+"""
+ * Any agent that wants to use GOAP must implement
+ * this interface. It provides information to the GOAP
+ * planner so it can plan what actions to use.
+ * 
+ * It also provides an interface for the planner to give 
+* feedback to the Agent and report success/failure.
+"""
+
+"""
+	 * The starting state of the Agent and the world.
+	 * Supply what states are needed for actions to run.
+"""
+func get_world_state() -> Dictionary:
+	#OVERRIDE THIS
+	return {}
+
+"""
+	 * Give the planner a new goal so it can figure out 
+	 * the actions needed to fulfill it.
+"""
+func create_goal_state() -> Dictionary:
+	#OVERRIDE THIS
+	return {}
+
+"""
+	 * No sequence of actions could be found for the supplied goal.
+	 * You will need to try another goal
+"""
+func plan_failed(failed_goal : Dictionary) -> void:
+	#OVERRIDE THIS
+	pass
+
+"""
+	 * A plan was found for the supplied goal.
+	 * These are the actions the Agent will perform, in order.
+"""
+func planFound(goal : Dictionary, actions : Array) -> void: #(HashSet<KeyValuePair<string,object>> goal, Queue<GoapAction> actions)
+	#OVERRIDE THIS
+	pass
+
+"""
+	 * All actions are complete and the goal was reached. Hooray!
+"""
+func actions_finished() -> void:
+	#OVERRIDE THIS
+	pass
+
+"""
+	 * One of the actions caused the plan to abort.
+	 * That action is returned.
+"""
+func plan_aborted(aborter : GoapAction) -> void:
+	#OVERRIDE THIS
+	pass
+
+"""
+	 * Called during Update. Move the agent towards the target in order
+	 * for the next action to be able to perform.
+	 * Return true if the Agent is at the target and the next action can perform.
+	 * False if it is not there yet.
+"""
+func move_agent(next_action : GoapAction) -> bool:
+	#OVERRIDE THIS
+	return false
