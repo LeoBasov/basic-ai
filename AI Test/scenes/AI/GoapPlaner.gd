818aee99fd6f1ad524b17d321a33be95d232a2ac
Leo Basov <basov_leo@web.de>
2019-11-23 16:57:07 +0100

    Added GoapPlaner

---
+++ AI Test/scenes/AI/GoapPlaner.gd
@@ -0,0 +1,51 @@
+extends Node
+
+"""
+In node class Node. This needs another name.
+Used for building up the graph and holding the running costs of actions.
+"""
+
+"""
+Plan what sequence of actions can fulfill the goal.
+Returns null if a plan could not be found, or a list of the actions
+that must be performed, in order, to fulfill the goal.
+Returns list of GoapActions
+"""
+func plan(agent, availableActions : Dictionary,  worldState : Dictionary, goal : Dictionary) -> Array:
+	#FILL IN FUNCTION
+	return []
+	
+"""
+Returns true if at least one solution was found.
+The possible paths are stored in the leaves list. Each leaf has a
+'runningCost' value where the lowest cost will be the best action
+sequence.
+
+parent : Node new type defined at the buttom of the class
+"""
+func buildGraph (parent, leaves : Array, usableActions : Array, goal : Dictionary) -> bool:
+	#FILL IN FUNCTION
+	return false
+	
+"""
+Create a subset of the actions excluding the removeMe one. Creates a new set.
+"""
+func actionSubset(actions : Array, removeMe : GoapAction) -> Array:
+	#FILL IN FUNCTION
+	return []
+
+"""
+private class Node {
+		public Node parent;
+		public float runningCost;
+		public HashSet<KeyValuePair<string,object>> state;
+		public GoapAction action;
+
+		public Node(Node parent, float runningCost, HashSet<KeyValuePair<string,object>> state, GoapAction action) {
+			this.parent = parent;
+			this.runningCost = runningCost;
+			this.state = state;
+			this.action = action;
+		}
+	}
+"""
