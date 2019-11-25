7ea63571d43c8429963077d5053852f30a23e1a3
Leo Basov <l.basov@sparc-industries.com>
2019-11-25 13:06:17 +0100

    First implementation of PerformActionState

--- AI Test/scenes/AI/PerformActionState.gd
+++ AI Test/scenes/AI/PerformActionState.gd
@@ -1,5 +1,37 @@
 extends "res://scenes/AI/FiniteState.gd"
 
+var data_provider
+var current_actions
+
 func update(object):
-	print(name)
-	emit_signal("pop")
+	var action : GoapAction = current_actions.back()
+	
+	if action.isDone():
+		# the action is done. Remove it so we can perform the next one
+		current_actions.pop_back()
+
+	if current_actions.size() != 0:
+		# perform the next action
+		action = current_actions.back()
+		var inRange : bool = action.isInRange() if action.requiresInRange() else true
+
+		if inRange:
+			# we are in range, so perform the action
+			var success : bool = action.perform(data_provider)
+
+			if !success:
+				# action failed, we need to plan again
+				emit_signal("pop")
+				emit_signal("push", "IdleState")
+				data_provider.planAborted(action)
+
+		else:
+			# we need to move there first
+			# push moveTo state
+			emit_signal("push", "MoveToState")
+
+	else:
+		# no actions left, move to Plan state
+		emit_signal("pop")
+		emit_signal("push", "IdleState")
+		data_provider.actionsFinished()
