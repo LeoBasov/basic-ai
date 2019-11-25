dea6f308c327118a48c4a7a0b3be2c0900416953
Leo Basov <l.basov@sparc-industries.com>
2019-11-25 15:35:32 +0100

    Added movement

--- AI Test/scenes/creeps/goap/IGoap.gd
+++ AI Test/scenes/creeps/goap/IGoap.gd
@@ -6,3 +6,14 @@ func get_world_state() -> Dictionary:
 	goal["get_food"] = true
 	
 	return goal
+	
+func move_agent(next_action : GoapAction) -> bool:
+	var vec = next_action.target.position - game_object.position
+	var velocity = vec.normalized()*game_object.SPEED
+	game_object.move_and_collide(velocity)
+	
+	if (next_action.target.position - game_object.position).length() < 10.0:
+		next_action.in_range = true
+		return true
+	else:
+		return false
