14f07b1e65c5f74dda45433ff17eb96550079bc9
Leo Basov <l.basov@sparc-industries.com>
2019-11-25 13:52:18 +0100

    Added stuff

--- AI Test/scenes/AI/MoveState.gd
+++ AI Test/scenes/AI/MoveState.gd
@@ -1,32 +1,16 @@
 extends "res://scenes/AI/FiniteState.gd"
 
-var move_is_possible : bool
-var move_complete : bool
-
-signal check_if_move_possible()
-signal move_agent()
-
-func _ready():
-	self.move_is_possible = false
-	self.move_complete = false
-	
-func set_move_is_possible(move_is_possible : bool) -> void:
-	self.move_is_possible = move_is_possible
-
-func set_move_complete(move_complete : bool) -> void:
-	self.move_complete = move_complete
-
 func update(object):
-	emit_signal("check_if_move_possible")
+	var data_provider = object.get_node("GoapAgent/IGoap")
+	var current_actions = object.get_node("GoapAgent").current_actions
 	
-	if !self.move_is_possible:
+	var action : GoapAction = current_actions.back()
+	
+	if action.requires_in_range() and action.target == null:
 		emit_signal("pop")
 		emit_signal("pop")
 		emit_signal("push", "IdleState")
 		return
-
-	#get the agent to move itself
-	emit_signal("move_agent")
 	
-	if self.move_complete:
+	if data_provider.moveAgent(action) :
 		emit_signal("pop")
