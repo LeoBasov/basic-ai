f117f9b8c35451108a6120fc86856c4a92518977
Leo Basov <l.basov@sparc-industries.com>
2019-11-25 14:25:53 +0100

    Added stuff.

--- AI Test/scenes/AI/GoapPlaner.gd
+++ AI Test/scenes/AI/GoapPlaner.gd
@@ -8,9 +8,58 @@ Returns null if a plan could not be found, or a list of the actions
 that must be performed, in order, to fulfill the goal.
 Returns list of GoapActions
 """
-func plan(agent, availableActions : Array,  worldState : Dictionary, goal : Dictionary) -> Array:
-	#FILL IN FUNCTION
-	return []
+func plan(agent, available_actions : Array,  world_state : Dictionary, goal : Dictionary) -> Array:
+	
+	for action in available_actions:
+		action.do_reset()
+		
+	# check what actions can run using their checkProceduralPrecondition
+	var usableActions : Array = []
+	
+	for action in available_actions:
+		if action.check_procedural_precondition(agent):
+			available_actions.push_back(action);
+			
+	# we now have all actions that can run, stored in usableActions
+
+	# build up the tree and record the leaf nodes that provide a solution to the goal.
+	var leaves : Array = []
+
+	# build graph
+	var start : GoapPlanerNode = GoapPlanerNode.new(null, 0, world_state, null)
+	var success : bool = build_graph(start, leaves, usableActions, goal)
+
+	if !success:
+		# oh no, we didn't get a plan
+		return []
+		
+	# get the cheapest leaf
+	var cheapest = null
+	for leaf in leaves:
+		if cheapest == null:
+			cheapest = leaf
+		else:
+			if leaf.running_cost < cheapest.running_cost:
+				cheapest = leaf;
+				
+	# get its node and work back through the parents
+	var result : Array = []
+	var n = cheapest
+	
+	while n != null:
+		if n.action != null:
+			result.push_front(n.action) # insert the action in the front
+
+		n = n.parent;
+	# we now have this action list in correct order
+	
+	var  queue : Array = []
+	
+	for action in result:
+		queue.push_back(action)
+
+	# hooray we have a plan!
+	return queue;
 	
 """
 Returns true if at least one solution was found.
--- AI Test/scenes/AI/GoapPlanerNode.gd
+++ AI Test/scenes/AI/GoapPlanerNode.gd
@@ -7,12 +7,13 @@ Used for building up the graph and holding the running costs of actions.
 
 class_name GoapPlanerNode
 
+var parent
 var running_cost : float
 var state : Dictionary
 var action : GoapAction
 
-func _init(parent : GoapPlanerNode, running_cost : float, state : Dictionary, action : GoapAction):
-	self.parent = parent;
-	self.running_cost = running_cost;
-	self.state = state;
-	self.action = action;
+func _init(parent, running_cost : float, state : Dictionary, action : GoapAction):
+	self.parent = parent
+	self.running_cost = running_cost
+	self.state = state
+	self.action = action
